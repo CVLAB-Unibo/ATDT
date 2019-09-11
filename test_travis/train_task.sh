660c0a15d45eed5756b20e24cde0484601974d88
pierlui92 <zamapierluigi@hotmail.it>
2019-09-11 14:58:46 +0200

    update

--- test_travis/train_task.sh
+++ test_travis/train_task.sh
@@ -1,1 +1,1 @@
-python3 train_task.py --data_path "" --input_list test_travis/list.txt --checkpoint_dir test_travis "tmp_log" --steps 1 --batch_size 1 --task "semantic" --normalizer_fn batch_norm --crop --crop_w 512 --crop_h 512
+python3 train_task.py --data_path "" --input_list test_travis/list.txt --checkpoint_dir "test_travis/tmp_log" --steps 1 --batch_size 1 --task "semantic" --normalizer_fn batch_norm --crop --crop_w 512 --crop_h 512
